fe59c7a9b0bb19b5258e8d97903844e38e8b28e7
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-07-14 11:22:05 +0200

    pipe type generic

--- src/app/filtro-prodotti.pipe.ts
+++ src/app/filtro-prodotti.pipe.ts
@@ -1,15 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Product, ProductsRoot } from './models';
 
 @Pipe({
   name: 'filtroProdotti'
 })
-export class FiltroProdottiPipe implements PipeTransform {
-
-  transform(value: Product[] | undefined, filtro: string): Product[] | undefined {
-    if (filtro === '' || undefined) return value;
-    const filteredProducts = value?.filter(p => p.category === filtro);
+export class FiltroProdottiPipe<T> implements PipeTransform {
+  transform(value: T[] | undefined, filtro: string, key: keyof T): T[] | undefined {
+    if (!filtro) return value;
+    const filteredProducts = value?.filter(p => p[key] === filtro);
     return filteredProducts;
   }
-
 }
